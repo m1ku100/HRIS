1fd6e7251bccc44776f5017d32d3205580d6aa28
m1ku100 <ilham.puji100@gmail.com>
2018-07-26 18:38:39 +0700

    Add Db & Model Skill

---
+++ app/Skill.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Skill extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'skill';
+
+    protected $fillable = [
+        'id', 'tingkat', 'deskripsi',
+    ];
+
+    protected $dates = ['deleted_at'];
+}
---
+++ database/migrations/2018_07_26_111314_create_skills_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skill', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tingkat');
+            $table->text('deskripsi');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
