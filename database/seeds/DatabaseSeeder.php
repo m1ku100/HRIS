d6b5a0a45c4adaf27c303422341b07b911f22e36
m1ku100 <ilham.puji100@gmail.com>
2018-07-26 19:15:45 +0700

    Add Negara Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(EduSeeder::class);
         $this->call(PosisiSeeder::class);
+        $this->call(NegaraSeeder::class);
 
     }
 }
