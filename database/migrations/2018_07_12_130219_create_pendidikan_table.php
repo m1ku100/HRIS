8081e0cb4bc55ad12fa6bdb6770c602d5c37caef
m1ku100 <ilham.puji100@gmail.com>
2018-07-27 20:50:29 +0700

    Minor Update Pendidikan DB

--- database/migrations/2018_07_12_130219_create_pendidikan_table.php
+++ database/migrations/2018_07_12_130219_create_pendidikan_table.php
@@ -20,6 +20,7 @@ class CreatePendidikanTable extends Migration
             $table->integer('edu_id')->unsigned();
             $table->foreign('edu_id')->references('id')->on('edu')->onUpdate('CASCADE');
             $table->string('instansi');
+            $table->string('jurusan');
             $table->integer('negara_id')->unsigned();
             $table->foreign('negara_id')->references('id')->on('negara')->onUpdate('CASCADE');
             $table->string('tahun_lulus');
